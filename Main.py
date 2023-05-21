216a4ef3a63d0b9b04d5d22a48335e667217b694
Hazumiran <diazazhari32@gmail.com>
2023-05-21 15:08:15 +0700

    nambahin requirements

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 # from fer import Video
 # from fer import FER
-import os
-import tempfile
+# import os
+# import tempfile
 
 page_bg_img = """
 <style>
