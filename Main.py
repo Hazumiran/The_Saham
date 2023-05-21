32f2871f06f10284cb763d2133020cbdf0ae7554
Hazumiran <diazazhari32@gmail.com>
2023-05-21 14:01:22 +0700

    nambahin requirements

--- Main.py
+++ Main.py
@@ -1,21 +1,9 @@
-
-
-from contextlib import contextmanager
-from io import StringIO
-# from streamlit.report_thread import REPORT_CONTEXT_ATTR_NAME
-from threading import current_thread
 import streamlit as st
-import json
-import requests
 from streamlit_option_menu import option_menu
-from streamlit_lottie import st_lottie
-from fer import Video
-from fer import FER
-from pathlib import Path
-import matplotlib.pyplot as plt
-import os
-import sys
-import tempfile
+# from fer import Video
+# from fer import FER
+# import os
+# import tempfile
 
 page_bg_img = """
 <style>
@@ -39,34 +27,34 @@ selected = option_menu(
 )
 
 # Menambahkan judul dan deskripsi
-st.title("Challenges in Representation Learning: A report on three machine learning contests")
+# st.title("Challenges in Representation Learning: A report on three machine learning contests")
 
-uploaded_file = st.file_uploader(
-    "Silahkan Upload Video disini", type=["mp4", "mkv", "mov"], accept_multiple_files=False)
+# uploaded_file = st.file_uploader(
+#     "Silahkan Upload Video disini", type=["mp4", "mkv", "mov"], accept_multiple_files=False)
 
-st.video(uploaded_file)
+# st.video(uploaded_file)
 
-if uploaded_file is None:
-    st.warning("Please upload a video file.")
-else:
-    with st.spinner('Wait for it...'):
+# if uploaded_file is None:
+#     st.warning("Please upload a video file.")
+# else:
+#     with st.spinner('Wait for it...'):
 
-        detector = FER(mtcnn=True)
-        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-            tmp_file.write(uploaded_file.read())
-            video_path = tmp_file.name
-            video = Video(video_path)
-        if not os.path.exists(video_path):
-            st.warning("Video file not found.")
-        else:
-            raw_data = video.analyze(detector, display=False)
-            # continue with analysis
-        os.unlink(video_path)  # delete temporary file
-    st.success('Done!')
-    df = video.to_pandas(raw_data)
-    df = video.get_first_face(df)
-    df = video.get_emotions(df)
+#         detector = FER(mtcnn=True)
+#         with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+#             tmp_file.write(uploaded_file.read())
+#             video_path = tmp_file.name
+#             video = Video(video_path)
+#         if not os.path.exists(video_path):
+#             st.warning("Video file not found.")
+#         else:
+#             raw_data = video.analyze(detector, display=False)
+#             # continue with analysis
+#         os.unlink(video_path)  # delete temporary file
+#     st.success('Done!')
+#     df = video.to_pandas(raw_data)
+#     df = video.get_first_face(df)
+#     df = video.get_emotions(df)
 
-    # st.image(fig)
-    st.write(df)
-    st.line_chart(df)
+#     # st.image(fig)
+#     st.write(df)
+#     st.line_chart(df)
